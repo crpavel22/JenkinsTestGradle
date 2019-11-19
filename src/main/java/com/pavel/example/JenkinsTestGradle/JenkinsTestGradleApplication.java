28afdf6602819185e77f9c26e8c51bdd9a56d477
crpavel <crpavel@icloud.com>
2019-11-19 11:47:16 -0500

    First Charge

---
+++ src/main/java/com/pavel/example/JenkinsTestGradle/JenkinsTestGradleApplication.java
@@ -0,0 +1,13 @@
+package com.pavel.example.JenkinsTestGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavel/example/JenkinsTestGradle/JenkinsTestGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavel.example.JenkinsTestGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
