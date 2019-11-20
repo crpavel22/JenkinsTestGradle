538d4eb8d3d80c06c78846a3c8cae39d8ef91ac8
crpavel <crpavel@icloud.com>
2019-11-20 15:57:11 -0500

    Change Jenkins file

--- src/main/java/com/pavel/example/JenkinsTestGradle/JenkinsTestGradleApplication.java
+++ src/main/java/com/pavel/example/JenkinsTestGradle/JenkinsTestGradleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsTestGradleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hola munod!!!!!!!");
 		SpringApplication.run(JenkinsTestGradleApplication.class, args);
 	}
 
